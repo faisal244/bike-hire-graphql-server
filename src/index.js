d4eebf886bb27d40b5f466d2e66cfdb9d94003c6
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-08-04 17:24:22 +0100

    Added connection code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello from node");
+require("dotenv").config();
+
+const connectToDatabase = require("./config/connection");
+
+const PORT = process.env.PORT || 4000;
+
+const init = async () => {
+	// establish a connection with database
+	await connectToDatabase();
+
+	// create a graphQL server
+
+	// start your graphQL server
+};
+
+init();
